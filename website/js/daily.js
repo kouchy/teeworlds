b803a98f8bec1b86b3b4f508c3e4e4f654f74c9a
Philippe Virouleau <philippe.44@gmail.com>
2018-07-12 15:37:24 +0200

    rework dashboard

--- website/js/daily.js
+++ website/js/daily.js
@@ -1,5 +1,5 @@
 //main
-$(document).ready(function() {
+$(function() {
 	var currentTime = new Date();
 	var month = currentTime.getMonth() +1;
 	var day = currentTime.getDate();
--- website/js/dashboard.js
+++ website/js/dashboard.js
@@ -1,224 +1,135 @@
-function notify_new_player(player, status) {
+var weapons = ["hammer", "gun", "shotgun", "grenade", "laser", "ninja"];
+var items = ['heart', 'armor', 'shotgun', 'grenade', 'laser', 'ninja'];
+var player_stats = ['kill', 'death', 'suicide'];
+var flag_stats = ['capture', 'grab', 'return', 'min_time'];
+// only 'armor' is not simply capitalized
+var items_names = { 'armor': 'Shield' }
 
-  var title = "Teeworlds Inria/IMS";
-  var options = {
-      body: player + " joined the " + status + " team!",
-      icon: "images/tee_small.png"
-  }
-
-  // Let's check if the browser supports notifications
-  if (!("Notification" in window)) {
-    console.log("This browser does not support desktop notification");
-  }
-
-  // Let's check whether notification permissions have already been granted
-  else if (Notification.permission === "granted") {
-    // If it's okay let's create a notification
-    var notification = new Notification(title, options);
-  }
-
-  // Otherwise, we need to ask the user for permission
-  else if (Notification.permission !== 'denied' || Notification.permission === "default") {
-    Notification.requestPermission(function (permission) {
-      // If the user accepts, let's create a notification
-      if (permission === "granted") {
-        var notification = new Notification(title, options);
-      }
-    });
-  }
-
-  // At last, if the user has denied notifications, and you
-  // want to be respectful there is no need to bother them any more.
+function capitalize(string) {
+	return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
-var pseudo_online_prev = [];
-var pseudo_online_red_prev = [];
-var pseudo_online_blue_prev = [];
-var pseudo_online_spectator_prev = [];
+// Create a new empty stat object to hold the stats
+function get_new_empty_stats() {
+	let schema = {
+		kill: { number: [], weapon: {}, },
+		death: { number: [], weapon: {}, },
+		suicide: { number: [], },
+		item: {},
+		flag: {},
+		game: { time: [], },
+	};
+	// Fill up with info about stats
+	weapons.forEach(w => {
+		schema.kill.weapon[w] = [];
+		schema.death.weapon[w] = [];
+	})
+	items.forEach(i => schema.item[i] = []);
+	flag_stats.forEach(f => schema.flag[f] = []);
+	return schema;
+}
+
+function reduce_stat(data, element, stat_paths = []) {
+	// Recursively accumulate each stat.
+	// At the end of all this, each stat will be a vector matching the players' vector
+	Object.keys(element).forEach(substat => {
+		if (Array.isArray(element[substat])) {
+			let new_stat = data;
+			stat_paths.forEach(path => new_stat = new_stat[path]);
+			element[substat].push(new_stat[substat]);
+		} else {
+			reduce_stat(data, element[substat], stat_paths.concat(substat));
+		}
+	});
+}
+
+// Combines two arrays of matching sizes 'keys' and 'values' into an object, applying 'filter_func' on each value.
+function to_object(keys, values, filter_func = () => true) {
+  let object = {};
+  for (let i = 0; i < keys.length; i++)
+		if (filter_func(values[i]))
+    	object[keys[i]] = values[i];
+  return object;
+};
 
 function draw_dashboard(filename, update = false)
 {
-	if (update == false)
-	{
+	if (!update) {
 		$("#loader").show();
-		$("#kill_death_suicide").empty();
-		$("#chrono").empty();
-		$("#kill_weapon").empty();
-		$("#death_weapon").empty();
-		$("#item").empty();
-		$("#flag").empty();
-		$("#time").empty();
-		$("#raw_json").empty();
+		["kill_death_suicide", "chrono", "kill_weapon", "death_weapon", "item", "flag", "time", "raw_json"].forEach(e => $(`#${e}`).empty());
 		$("#error").hide();
 	}
 
 	$.getJSON( filename, function( data ) {
-		var pseudo = [];
-		var kill = [];
-		var death = [];
-		var suicide = [];
-		var kill_weapon_hammer = [];
-		var kill_weapon_gun    = [];
-		var kill_weapon_shotgun = [];
-		var kill_weapon_grenade = [];
-		var kill_weapon_laser = [];
-		var kill_weapon_ninja = [];
-		var death_weapon_hammer = [];
-		var death_weapon_gun    = [];
-		var death_weapon_shotgun = [];
-		var death_weapon_grenade = [];
-		var death_weapon_laser = [];
-		var death_weapon_ninja = [];
-		var pseudo_chrono = [];
-		var chrono = [];
-		var item_heart = [];
-		var item_armor = [];
-		var item_shotgun = [];
-		var item_grenade = [];
-		var item_laser = [];
-		var item_ninja = [];
-		var flag_capture = [];
-		var flag_grab = [];
-		var flag_return = [];
-		var time = [];
-		var n_online = 0;
-		var pseudo_online = [];
-		var pseudo_online_red = [];
-		var pseudo_online_blue = [];
-		var pseudo_online_spectator = [];
-		$.each( data, function( key, val ) {
-			pseudo.push(key);
-			kill.push(data[key].kill.number);
-			death.push(data[key].death.number);
-			suicide.push(data[key].suicide.number);
-			kill_weapon_hammer.push(data[key].kill.weapon.hammer);
-			kill_weapon_gun.push(data[key].kill.weapon.gun);
-			kill_weapon_shotgun.push(data[key].kill.weapon.shotgun);
-			kill_weapon_grenade.push(data[key].kill.weapon.grenade);
-			kill_weapon_laser.push(data[key].kill.weapon.laser);
-			kill_weapon_ninja.push(data[key].kill.weapon.ninja);
-			death_weapon_hammer.push(data[key].death.weapon.hammer);
-			death_weapon_gun.push(data[key].death.weapon.gun);
-			death_weapon_shotgun.push(data[key].death.weapon.shotgun);
-			death_weapon_grenade.push(data[key].death.weapon.grenade);
-			death_weapon_laser.push(data[key].death.weapon.laser);
-			death_weapon_ninja.push(data[key].death.weapon.ninja);
-			if (data[key].flag.min_time > 0)
-			{
-				pseudo_chrono.push(key);
-				chrono.push(data[key].flag.min_time);
-			}
-			item_heart.push(data[key].item.heart);
-			item_armor.push(data[key].item.armor);
-			item_shotgun.push(data[key].item.shotgun);
-			item_grenade.push(data[key].item.grenade);
-			item_laser.push(data[key].item.laser);
-			item_ninja.push(data[key].item.ninja);
-			flag_capture.push(data[key].flag.capture);
-			flag_grab.push(data[key].flag.grab);
-			flag_return.push(data[key].flag.return);
-			time.push(Math.round(data[key].game.time/60.0));
-			if (data[key].game.team == "red"       ) { pseudo_online_red.push(key);       n_online++; }
-			if (data[key].game.team == "blue"      ) { pseudo_online_blue.push(key);      n_online++; }
-			if (data[key].game.team == "spectators") { pseudo_online_spectator.push(key); n_online++; }
-			if (data[key].game.team == "online"    ) { pseudo_online.push(key);           n_online++; }
-		});
+		let all_players = Object.keys(data);
+		let all_stats_by_type = get_new_empty_stats();
+
+		// Recursively browse the json and accumulate data
+		all_players.forEach(pseudo => reduce_stat(data[pseudo], all_stats_by_type));
+		// Convert times to minute
+		all_stats_by_type.game.time = all_stats_by_type.game.time.map(t => Math.round(t/60.0));
+		// Get and filter out non positive flag time
+		let chronos = to_object(all_players, all_stats_by_type.flag.min_time, time => time > 0);
+
+		let player_statuses = { "red": [], "blue": [], "online": [], "spectators": [], "offline": [] };
+		all_players.forEach(pseudo => player_statuses[data[pseudo].game.team || "offline"].push(pseudo));
+		let n_online = 0;
+		let online_statuses = {
+			red: { class: "danger", title: "In-game (red team)" },
+			blue: { class: "primary", title: "In-game (blue team)" },
+			online: { class: "warning", title: "In-game" },
+			spectators: { class: "secondary", title: "Spectator" },
+		};
+		Object.keys(online_statuses).forEach(status => n_online += player_statuses[status].length);
 
 		$("#online").empty();
-		if (n_online)
-			$("#online").append(n_online + " player" + (n_online > 1 ? "s" : "") + " online : ");
-		for (var i=0;i<pseudo_online_red.length;i++)
-		{
-			$("#online").append("<span class=\"badge badge-danger\"><abbr title=\"In-game (red team)\">" + pseudo_online_red[i] + "</abbr></span>&nbsp;");
-			if (update && (pseudo_online_red_prev.indexOf(pseudo_online_red[i]) == -1))
-				notify_new_player(pseudo_online_red[i], "red");
+		if (n_online) {
+			$("#online").append(`${n_online} player${n_online > 1 ? "s" : ""} online: `);
+			Object.entries(online_statuses).forEach(entry => {
+				player_statuses[entry[0]].forEach(player => {
+					$("#online").append(`<span class="badge badge-${entry[1].class}"><abbr title="${entry[1].title}">${player}</abbr></span>&nbsp;`);
+				});
+			});
 		}
 
-		for (var i=0;i<pseudo_online_blue.length;i++)
-		{
-			$("#online").append("<span class=\"badge badge-primary\"><abbr title=\"In-game (blue team)\">" + pseudo_online_blue[i] + "</abbr></span>&nbsp;");
-			if (update && (pseudo_online_blue_prev.indexOf(pseudo_online_blue[i]) == -1))
-				notify_new_player(pseudo_online_blue[i], "blue");
-		}
-		for (var i=0;i<pseudo_online.length;i++)
-		{
-			$("#online").append("<span class=\"badge badge-warning\"><abbr title=\"In-game\">" + pseudo_online[i] + "</abbr></span>&nbsp;");
-			if (update && (pseudo_online_prev.indexOf(pseudo_online[i]) == -1))
-				notify_new_player(pseudo_online[i], "general");
-		}
-		for (var i=0;i<pseudo_online_spectator.length;i++)
-		{
-			$("#online").append("<span class=\"badge badge-secondary\"><abbr title=\"Spectator\">" + pseudo_online_spectator[i] + "</abbr></span>&nbsp;");
-			if (update && (pseudo_online_spectator_prev.indexOf(pseudo_online_spectator[i]) == -1))
-				notify_new_player(pseudo_online_spectator[i], "spectator");
-		}
-
-		pseudo_online_red_prev = pseudo_online_red;
-		pseudo_online_blue_prev = pseudo_online_blue;
-		pseudo_online_prev = pseudo_online;
-		pseudo_online_spectator_prev = pseudo_online_spectator;
-
-		if (update == false)
+		if (!update)
 			$("#loader").hide();
 
-		var kill    = { x: pseudo, y: kill,    name: 'Kill',    type: 'bar' };
-		var death   = { x: pseudo, y: death,   name: 'Death',   type: 'bar' };
-		var suicide = { x: pseudo, y: suicide, name: 'Suicide', type: 'bar' };
-		var data_kill_death_suicide = [kill, death, suicide];
-		var layout_kill_death_suicide = {barmode: 'group', title: 'Kills, deaths and suicides', autorange: true};
-		Plotly.newPlot('kill_death_suicide', data_kill_death_suicide, layout_kill_death_suicide);
+		plots = [
+			{
+				elem: 'kill_death_suicide', title: 'Kills, deaths and suicides', stats: player_stats,
+				create_stat: stat => ({ x: all_players, y: all_stats_by_type[stat].number, name: capitalize(stat), type: 'bar' }),
+			},
+			{
+				elem: 'kill_weapon', title: 'Kills by weapons', stats: weapons,
+				create_stat: weapon => ({ x: all_players, y: all_stats_by_type.kill.weapon[weapon], name: capitalize(weapon), type: 'bar' }),
+			},
+			{
+				elem: 'death_weapon', title: 'Deaths by weapons', stats: weapons,
+				create_stat: weapon => ({ x: all_players, y: all_stats_by_type.death.weapon[weapon], name: capitalize(weapon), type: 'bar' }),
+			},
+			{
+				elem: 'chrono', title: 'Chrono (shortest flag time in seconds)', stats: [0],
+				create_stat: () => ({ x: Object.keys(chronos), y: Object.values(chronos), name: 'Flag time (sec)', type: 'bar' }),
+			},
+			{
+				elem: 'item', title: 'Items count', stats: items,
+				create_stat: item => ({ x: all_players, y: all_stats_by_type.item[item], name: items_names[item] || capitalize(item), type: 'bar' }),
+			},
+			{
+				elem: 'flag', title: 'Flag actions', stats: flag_stats,
+				create_stat: action => ({ x: all_players, y: all_stats_by_type.flag[action], name: capitalize(action), type: 'bar' }),
+			},
+			{
+				elem: 'time', title: 'Time spent (in minutes)', stats: [0],
+				create_stat: () => ({ x: all_players, y: all_stats_by_type.game.time, name: 'Time (sec)', type: 'bar' }),
+			},
+		];
 
-		var kill_weapon_hammer  = { x: pseudo, y: kill_weapon_hammer,  name: 'Hammer',  type: 'bar' };
-		var kill_weapon_gun     = { x: pseudo, y: kill_weapon_gun,     name: 'Gun',     type: 'bar' };
-		var kill_weapon_shotgun = { x: pseudo, y: kill_weapon_shotgun, name: 'Shotgun', type: 'bar' };
-		var kill_weapon_grenade = { x: pseudo, y: kill_weapon_grenade, name: 'Grenade', type: 'bar' };
-		var kill_weapon_laser   = { x: pseudo, y: kill_weapon_laser,   name: 'Laser',   type: 'bar' };
-		var kill_weapon_ninja   = { x: pseudo, y: kill_weapon_ninja,   name: 'Ninja',	  type: 'bar' };
-		var data_kill_weapon = [kill_weapon_hammer, kill_weapon_gun, kill_weapon_shotgun, kill_weapon_grenade, kill_weapon_laser, kill_weapon_ninja];
-		var layout_kill_weapon = {barmode: 'group', title: 'Kills by weapons'};
-		Plotly.newPlot('kill_weapon', data_kill_weapon, layout_kill_weapon);
+		plots.forEach(plot_data => Plotly.newPlot(plot_data.elem, plot_data.stats.map(s => plot_data.create_stat(s)), { barmode: 'group', title: plot_data.title, autorange: true }));
 
-		var death_weapon_hammer  = { x: pseudo, y: death_weapon_hammer,  name: 'Hammer',  type: 'bar' };
-		var death_weapon_gun     = { x: pseudo, y: death_weapon_gun,     name: 'Gun',     type: 'bar' };
-		var death_weapon_shotgun = { x: pseudo, y: death_weapon_shotgun, name: 'Shotgun', type: 'bar' };
-		var death_weapon_grenade = { x: pseudo, y: death_weapon_grenade, name: 'Grenade', type: 'bar' };
-		var death_weapon_laser   = { x: pseudo, y: death_weapon_laser,   name: 'Laser',   type: 'bar' };
-		var death_weapon_ninja   = { x: pseudo, y: death_weapon_ninja,   name: 'Ninja',   type: 'bar' };
-		var data_death_weapon = [death_weapon_hammer, death_weapon_gun, death_weapon_shotgun, death_weapon_grenade, death_weapon_laser, death_weapon_ninja];
-		var layout_death_weapon = {barmode: 'group', title: 'Deaths by weapons'};
-		Plotly.newPlot('death_weapon', data_death_weapon, layout_death_weapon);
-
-		var chrono = { x: pseudo_chrono, y: chrono, name: 'Flag time (sec)', type: 'bar' };
-		var data_chrono = [chrono];
-		var layout_chrono = {barmode: 'group', title: 'Chrono (shortest flag time in seconds)', autorange: true};
-		Plotly.newPlot('chrono', data_chrono, layout_chrono);
-
-		var item_heart   = { x: pseudo, y: item_heart,   name: 'Heart',   type: 'bar' };
-		var item_armor   = { x: pseudo, y: item_armor,   name: 'Shield',  type: 'bar' };
-		var item_shotgun = { x: pseudo, y: item_shotgun, name: 'Shotgun', type: 'bar' };
-		var item_grenade = { x: pseudo, y: item_grenade, name: 'Grenade', type: 'bar' };
-		var item_laser   = { x: pseudo, y: item_laser,   name: 'Laser',   type: 'bar' };
-		var item_ninja   = { x: pseudo, y: item_ninja,   name: 'Ninja',   type: 'bar' };
-		var data_item  = [item_heart, item_armor, item_shotgun, item_grenade, item_laser, item_ninja];
-		var layout_item = {barmode: 'group', title: 'Items count'};
-		Plotly.newPlot('item', data_item, layout_item);
-
-		var flag_capture = { x: pseudo, y: flag_capture,  name: 'Capture',    type: 'bar' };
-		var flag_grab    = { x: pseudo, y: flag_grab,     name: 'Grab',       type: 'bar' };
-		var flag_return  = { x: pseudo, y: flag_return,   name: 'Bring back', type: 'bar' };
-		var data_flag    = [flag_capture, flag_grab, flag_return];
-		var layout_flag  = {barmode: 'group', title: 'Flag actions'};
-		Plotly.newPlot('flag', data_flag, layout_flag);
-
-		var time = { x: pseudo, y: time,   name: 'Time (sec)',  type: 'bar' };
-		var data_time = [time];
-		var layout_time = {barmode: 'group', title: 'Time spent (in minutes)'};
-		Plotly.newPlot('time', data_time, layout_time);
-
-		if (update == false)
-			$("#raw_json").append("<a href=\"" + filename + "\" class=\"btn btn-primary\" role=\"button\" target=\"_blank\">Raw JSON data file</a>");
-
-		$("#error").hide();
+		if (!update)
+			$("#raw_json").append(`<a href="${filename}" class="btn btn-primary" role="button" target="_blank">Raw JSON data file</a>`);
 	});
 }
 
