fa746a0f12bbfe28cfd9593f6654edad3f958545
Adrien Cassagne <adrien.cassagne@free.fr>
2018-08-10 14:55:46 +0200

    Put back the online players.

--- website/js/admin.js
+++ website/js/admin.js
@@ -173,6 +173,6 @@ $(document).ready(function() {
 	let year = currentTime.getFullYear();
 	let path = "stats_" + year + (month < 10 ? "0" : "") + month + (day < 10 ? "0" : "") + day + "_" + g_map + ".json";
 	draw_form("../" + g_root_dir + "/total/" + path);
-	// draw_online_players("../" + g_root_dir + "/daily/" + path, true);
-	// setInterval(function() { draw_online_players("../" + g_root_dir + "/daily/" + path, false); }, 5000);
+	draw_online_players("../" + g_root_dir + "/daily/" + path, true);
+	setInterval(function() { draw_online_players("../" + g_root_dir + "/daily/" + path, false); }, 5000);
 });
